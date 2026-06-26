a21d144f9d4a0a1d200bd18bf8bb0561cab81eb4
AmanChauhan264 <neerajchauhanch112@gmail.com>
2026-06-26 15:10:41 +0530

    add user.model.js

--- src/controllers/healthcheck.controllers.js
+++ src/controllers/healthcheck.controllers.js
@@ -18,7 +18,7 @@ const healthCheck = async (req, res, next) => {
 const healthCheck = asyncHandler(async(req, res) => {
     res
     .status(200)
-    .json( new ApiResponse(200, {message:"Server is still running"
+    .json( new ApiResponse(200, {message:"Server is running"
         }))
     })
 
