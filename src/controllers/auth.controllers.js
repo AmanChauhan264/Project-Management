53c7140ac1e9c2e9ac84d8d78e3397be8a30573f
AmanChauhan264 <neerajchauhanch112@gmail.com>
2026-07-31 11:44:05 +0530

    push logout user code in auth.controllers

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -125,7 +125,34 @@ const {accessToken, refreshToken} = await generateAccessAndRefreshTokens(user._i
    
 })
 
+const logoutUser = asyncHandler(async(req,res)=>{
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set: {
+                refreshToken: ""
+            }
+        },
+        {
+            new: true
+        },
+    )
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+    return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(
+        new ApiResponse(200, {}, "User logged out")
+    )
+
+})
+
 export {
     registerUser,
-    login
+    login,
+    logoutUser
 };
