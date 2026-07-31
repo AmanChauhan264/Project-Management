ed8a4236624db2a5a77ecdc4379c267ca45f9563
AmanChauhan264 <neerajchauhanch112@gmail.com>
2026-07-31 11:44:42 +0530

    push middleware modified code

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,26 +1,39 @@
-import {User} from "../models/user.models";
-import { ApiError } from "../utils/api-error";
-import { asyncHandler } from "../utils/async-handler";
-import jwt from "JsonWebToken"
+import { User } from "../models/user.models.js";
+import { ApiError } from "../utils/api-error.js";
+import { asyncHandler } from "../utils/async-handler.js";
+import jwt from "jsonwebtoken";
 
+export const verifyJWT = asyncHandler(async (req, res, next) => {
+    const token =
+        req.cookies?.accessToken ||
+        req.header("Authorization")?.replace("Bearer ", "");
 
-export const verifyJWT = asyncHandler(async(req, res, next)=>{
-   const token =  req.cookie?.accessToken || req.header("Authorization")?.
-    replace("Bearer ", "")
+    console.log("Authorization Header =", req.header("Authorization"));
+    console.log("Token =", token);
+    console.log("Secret =", process.env.ACCESS_TOKEN_SECRET);
 
-    if(!token){
-        throw new ApiError(401, "Unauthorized request")
+    if (!token) {
+        throw new ApiError(401, "Unauthorized request");
     }
+
     try {
-        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
-      const user =   await User.finById(decodedToken?._id).select("-password -refreshToken -emailVerificationToken -emailVerificationExpiry")
+        const decodedToken = jwt.verify(
+            token,
+            process.env.ACCESS_TOKEN_SECRET
+        );
 
-       if(!user){
-        throw new ApiError(401, "Invalid access token")
-    }
-    req.user = user
-    next()
+        console.log("Decoded =", decodedToken);
+
+        const user = await User.findById(decodedToken?._id);
+
+        if (!user) {
+            throw new ApiError(401, "Invalid access token");
+        }
+
+        req.user = user;
+        next();
     } catch (error) {
+        console.log("JWT Error =", error.message);
         throw new ApiError(401, "Invalid access token");
     }
-})
+});
