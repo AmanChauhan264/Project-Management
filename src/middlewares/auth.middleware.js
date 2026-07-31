a7844bb9677dd56791b3fbb827dbf6974ce8adec
AmanChauhan264 <neerajchauhanch112@gmail.com>
2026-07-31 10:54:44 +0530

    push middleware code

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,26 @@
+import {User} from "../models/user.models";
+import { ApiError } from "../utils/api-error";
+import { asyncHandler } from "../utils/async-handler";
+import jwt from "JsonWebToken"
+
+
+export const verifyJWT = asyncHandler(async(req, res, next)=>{
+   const token =  req.cookie?.accessToken || req.header("Authorization")?.
+    replace("Bearer ", "")
+
+    if(!token){
+        throw new ApiError(401, "Unauthorized request")
+    }
+    try {
+        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+      const user =   await User.finById(decodedToken?._id).select("-password -refreshToken -emailVerificationToken -emailVerificationExpiry")
+
+       if(!user){
+        throw new ApiError(401, "Invalid access token")
+    }
+    req.user = user
+    next()
+    } catch (error) {
+        throw new ApiError(401, "Invalid access token");
+    }
+})
